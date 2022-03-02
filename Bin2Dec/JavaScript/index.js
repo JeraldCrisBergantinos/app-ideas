c8533f7b47f1ef1be1bb34f386f23e340a9402a4
JeraldCrisBergantinos <44352027+JeraldCrisBergantinos@users.noreply.github.com>
2022-03-02 20:57:14 +0800

    Added index.js.
    Added index.js.
    Added JavaScript implementation for binary to decimal.

---
+++ Bin2Dec/JavaScript/index.js
@@ -0,0 +1,7 @@
+function bin2Dec(bin) {
+    var decimal = 0;
+    for (var index = bin.length-1; index >= 0; index--) {
+        decimal += parseInt(bin[index]) * Math.pow(2, bin.length-1 - index);
+    }
+    return decimal;
+}
